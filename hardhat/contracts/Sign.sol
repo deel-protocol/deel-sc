20e8204eb298ea51b11094bf24209b582ddda798
Paul <mierlo.paul@hotmail.nl>
2024-09-08 00:41:10 +0200

    altered attestation SC

--- hardhat/contracts/Sign.sol
+++ hardhat/contracts/Sign.sol
@@ -9,14 +9,14 @@ import {DataLocation} from "@ethsign/sign-protocol-evm/src/models/DataLocation.s
 // [x] Have a way to link to the existing on-chain SP instance and schema
 // [x] Force both parties to confirm they've met each other IRL before making an attestation
 
-contract WorkerReputation is Ownable {
+contract JobCompletion is Ownable {
   ISP public spInstance;
   uint64 public schemaId;
-  mapping(uint256 data => address worker) public workerReputation;
+  mapping(address worker => address poster) public jobCompletion;
 
   error ConfirmationAddressMismatch();
 
-  event didAttestReputation(uint256 data, address worker, uint64 attestationId);
+  event didAttestWork(address poster, address worker, uint64 attestationId);
 
   constructor() Ownable(_msgSender()) {}
 
@@ -28,21 +28,21 @@ contract WorkerReputation is Ownable {
     schemaId = schemaId_;
   }
 
-  function verifyreputation(uint256 data) external {
-    workerReputation[data] = data;
+  function claimCompletedWork(address poster) external {
+    jobCompletion[poster] = poster;
   }
 
-  function confirmMetIRL(
-    address partyA,
+  function confirmCompletedWork(
+    address worker,
     bytes memory data
   ) external returns (uint64) {
-    address partyB = _msgSender();
-    if (metIRLMapping[partyA] == partyB) {
+    address poster = _msgSender();
+    if (jobCompletion[worker] == poster) {
       // B has confirm A's claim of having met them IRL
       // We now make an attestation of having actually met IRL
       bytes[] memory recipients = new bytes[](2);
-      recipients[0] = abi.encode(partyA);
-      recipients[1] = abi.encode(partyB);
+      recipients[0] = abi.encode(worker);
+      recipients[1] = abi.encode(poster);
       Attestation memory a = Attestation({
         schemaId: schemaId,
         linkedAttestationId: 0,
@@ -56,7 +56,7 @@ contract WorkerReputation is Ownable {
         data: data // SignScan assumes this is from `abi.encode(...)`
       });
       uint64 attestationId = spInstance.attest(a, "", "", "");
-      emit DidMeetIRL(partyA, partyB, attestationId);
+      emit DidMeetIRL(worker, poster, attestationId);
       return attestationId;
     } else {
       revert ConfirmationAddressMismatch();
